79b8a8e2605d699092a8fb6cfe14a8eb53f5cd91
Yiyun Qin <yiyun.qin@mths.ca>
2022-10-23 21:59:31 +0000

    linter

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * @author  Mr. Coxall
+ * @version 1.0
+ * @since   2020-01-01
+ */
+
+class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("\nDone.");
+  }
+}
